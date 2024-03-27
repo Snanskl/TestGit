af64f52ff3aeea0bcbcfdbe6a0a996cceb32e323
Ploypailin Junker <ploypilinjunker@hotmail.com>
2024-03-27 13:47:19 +0100

    More comments

--- FoodListReal.py
+++ FoodListReal.py
@@ -9,10 +9,12 @@ file_name = 'scraped_menu.csv'
 request = requests.get(base_url)
 soup = BeautifulSoup(request.content, 'html.parser')
 
-# Boolean operator if file exists or is empty (same directory as .py script)
+# Boolean operator if file exists or is empty (same directory as .py script) 
+#file_exists is True if file exists and is not empty
+#os = operating system
 file_exists = os.path.isfile(file_name) and os.path.getsize(file_name) > 0
 
-
+# a means append, newline='' is to avoid empty lines between rows
 with open(file_name, 'a', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     
@@ -25,12 +27,15 @@ with open(file_name, 'a', newline='', encoding='utf-8') as file:
     # Find every dish for each date
     headers = soup.find_all('thead', attrs={'class': 'lunch-day-header'})
 
+# Loop through each date and strip the text from thehtml tag
     for header in headers:
         dates_tag = header.find('h3')
         dates_text = dates_tag.text.strip()
    
         # Find next dish information within the same date
+        #find next sibling is a method that returns the tag that is right after the tag that is passed as an argument
         tbody = header.find_next_sibling('tbody', attrs={'class': 'lunch-day-content'})
+        #find_all returns a list of all the tags that match the criteria
         rows = tbody.find_all('tr', attrs={'class': 'lunch-menu-item'})
 
         for row in rows:
@@ -42,5 +47,5 @@ with open(file_name, 'a', newline='', encoding='utf-8') as file:
             price_tag = row.find('strong', attrs={'class': 'price-tag'})
             price_text = price_tag.text.strip()
 
-
+            #write the data to the csv file
             writer.writerow([dates_text, dishes_text, price_text])
